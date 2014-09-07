fc44d05702e332a9986133703604be480492362b
Christopher Celio <celio@eecs.berkeley.edu>
2014-09-07 03:04:12 -0700

    Now supports lspike "live" mode.

--- README.md
+++ README.md
@@ -6,10 +6,44 @@ strategies.
 
 In particular, BTB, BHT, and RAT interations with superscalar fetch units.
 
-To run it:
+Getting Started
+---------------
 
-    ./pythia
+*replay mode*
+
+    ./pythia -t vvadd
+
+*live mode*
+    
+    ./pythia -s ../riscv-tools/riscv-tests/benchmarks/vvadd.riscv
+
+Live mode assumes "lspike" is installed.
 
 For more information:
 
-   ./pythia -h
+    ./pythia -h
+
+
+There are *two* modes - "live" mode and "replay" mode.
+
+
+*"Live" mode* - is the default mode that invokes "lspike" and pipes the commit
+log to Pythia. use "-s" to specify the benchmark and its arguments. 
+
+*"Replay" mode* - use "-t" to name a tracefile in the "traces" directory to replay a run.
+
+Setting Up
+----------
+
+"Live" mode will require the riscv-tools to be installed. In particular, it
+needs to make use of the "commit logging" ability of "spike".
+
+1. Modify riscv-tool's "build.sh" to compile the "spike" ISA simulator into a new directory.
+
+    build_project riscv-fesvr --prefix=$RISCV/logger
+    build_project riscv-isa-sim --prefix=$RISCV/logger --with-fesvr=$RISCV/logger --enable-commitlog 
+
+2. Rename $RISCV/logger/bin/spike to $RISCV/logger/bin/lspike. 
+
+3. Finally, add $RISCV/logger to your bash environment path. 
+
--- pythia.py
+++ pythia.py
@@ -15,6 +15,8 @@
 # 0x0000000000002cdc (0xea5ff0ef) x 1 0x0000000000002ce0
 
 import optparse
+from subprocess import Popen, PIPE
+import shlex
 
 from btb import BTB
 from ras import RAS
@@ -41,21 +43,14 @@ def isBrOrJmp(inst):
    # branch
    if (opcode == 0x63):
       return 1
-   # jmp/jal (1101111)
+   # jmp/jal (110_1111)
    elif (opcode == 0x6f):
       return 2
-   # jalr (1100111)
+   # jalr (110_0111)
    elif (opcode == 0x67):
       return 3
    else:
       return 0
-
-# allow us to look at the next line in the file for matching against the target
-def peek_line(f):
-   pos = f.tell()
-   line = f.readline()
-   f.seek(pos)
-   return line
 
 def ParseLine(line):
    pc = int(line[2:18], 16)
@@ -76,23 +71,34 @@ def main():
    parser = optparse.OptionParser()
    parser.add_option('-d', '--debug', action="store_true", dest='debug',
                     help='Debug mode enabled')
+#   parser.add_option('-f', '--pathtobmarks', dest='bmarkpath',
+#                    help='Point to the directory (folder) of your benchmarks.', default="../riscv-tools/riscv-tests/benchmarks/")
+   parser.add_option('-s', '--benchmark', dest='benchmark',
+                    help='RISC-V benchmark to be run in "live" mode.', default="../riscv-tools/riscv-tests/benchmarks/vvadd.riscv")
    parser.add_option('-t', '--tracefile', dest='tracefile',
-                    help='input trace file [looks inside the trace directory], automatically adds .trace extension.', default="vvadd")
+                    help='input trace file for "replay" mode. Looks inside the trace directory and automatically adds .trace extension.', default="")
    parser.add_option('-p', '--predictor', dest='predictor',
-                    help='predictor', default="rocket")
+                    help='Choose your predictor.', default="rocket")
    parser.add_option('-w', '--width', dest='width',
-                    help='Processor fetch width', default=1)
+                    help='Processor fetch width.', default=1)
    parser.add_option('-b', '--btb-entries', dest='num_btb_entries',
                     help='Number of BTB entries', default=64)
    parser.add_option('-r', '--ras-entries', dest='num_ras_entries',
                     help='Number of RAS entries', default=2)
    (options, args) = parser.parse_args()
 
-#   if not options.filename:
-#      parser.error('Please give an input filename with -f')
-   
+   if options.width != 1:
+      parser.error('Only fetch widths of 1 are supported.')
 
-   trace = open("traces/" + options.tracefile + ".trace")
+
+   if (options.tracefile == ""):
+      cmd = "lspike " + options.benchmark
+      print cmd
+      trace = Popen(shlex.split(cmd), stderr=PIPE).stderr
+   else:
+      options.benchmark = options.tracefile
+      print "opening trace file (traces/%s.trace)" % options.tracefile
+      trace = open("traces/" + options.tracefile + ".trace")
 
    width = int(options.width)
 
@@ -101,14 +107,32 @@ def main():
    else:
       pred = RocketPredictor(width, int(options.num_btb_entries), int(options.num_ras_entries))
 
+   line_buffer = []
+   line_buffer.append(trace.readline())
+   buff_cnt = 1
+
 
    while 1:
-      line = trace.readline()
+      # fetch a bunch of instructions
+      if (buff_cnt < 2):
+
+         for i in range(0,2):
+            line_buffer.append(trace.readline())
+            buff_cnt += 1
+
+      if (buff_cnt > 0):
+         line = line_buffer.pop(0)
+         buff_cnt -= 1
+      else:
+         print "ERROR"
+         exit()
+
+
       if not line: break
 
       (pc, inst) = ParseLine(line)
       
-      #TODO add a predictWithDecodedInst, to experiment with BHT, RAS using decoded instructions
+      #TODO add a predictWithDecodedInst, to experiment with BHT, RAS using decoded instructions 
       (pred_taken, pred_target) = pred.predict(pc) 
 
 
@@ -116,16 +140,16 @@ def main():
       (is_ret, is_call) = isRetOrCall(br_type, inst)
      
       if (br_type > 0): 
-         # TODO "updateStats()"
+         # TODO use "updateStats()" method instead
          if (br_type==1): Stats.br += 1
          elif (br_type==2): Stats.jal += 1
          elif (br_type==3): Stats.jalr += 1
          else: print("error")
-
          if (is_ret): Stats.ret += 1
          elif (is_call): Stats.call += 1
 
-         next_line = peek_line(trace)
+         next_line = line_buffer[0]
+         if not next_line: break
          (target, n_inst) = ParseLine(next_line)
 
          was_taken = False 
@@ -147,8 +171,6 @@ def main():
 
          if (is_ret and was_mispredicted):
             Stats.missed_ret += 1
-#         elif (is_ret and pred_taken == was_taken and pred_target == target):
-#            print "yah"
 
          # Update
          ret_addr = pc+4
@@ -163,14 +185,6 @@ def main():
                                                                      ("PT" if pred_taken else "nT"),
                                                                      ("MISPREDICT" if was_mispredicted else " ")
                                                                      )
-            #if (was_mispredicted and was_taken):
-            #   print "pc: %08x, inst: %08x %d , target: %x MISPREDICT (%d, %x)" % (pc, inst, isBrOrJmp(inst), target, pred_taken, pred_target)
-            #elif (was_mispredicted and  not (was_taken)):
-            #   print "pc: %08x, inst: %08x %d , target: %x MISPREDICT (%d, %x) PC+4" % (pc, inst, isBrOrJmp(inst), target, pred_taken, pred_target)
-            #elif (was_taken):
-            #   print "pc: %08x, inst: %08x %d , target: %x TAKEN" % (pc, inst, isBrOrJmp(inst), target)
-            #else:
-            #   print "pc: %08x, inst: %08x %d , target: %x PC+4" % (pc, inst, isBrOrJmp(inst), target)
       else:
          if (options.debug):
             print "pc: 0x%08x, inst: %08x %d"  % (pc, inst, isBrOrJmp(inst))
@@ -179,9 +193,11 @@ def main():
    #---------------------------------------------------
 
    total = Stats.br + Stats.jal + Stats.jalr 
+   if (total == 0):
+      exit("Huh? Total branch/jmp count is zero!")
 
    print "\n=============================="
-   print "  Stats (%s): " % options.tracefile
+   print "  Stats (%s): " % options.benchmark
    print "   Total         : %6d   " % (total)
    print "     - br        : %6d  [%7.3f %%] " % (Stats.br  , 100.*Stats.br/total)
    print "     - jal       : %6d  [%7.3f %%] " % (Stats.jal , 100.*Stats.jal/total)
@@ -205,12 +221,12 @@ def main():
    # and is from the uarch counters, which are captured before the branch-heavy
    # printf code is called.
    if (options.predictor == "rocket"):
-      if (options.tracefile == "median"):    print "Median = 82.5% misp = 330, bj = 1888"
-      if (options.tracefile == "multiply"):  print "Multiply = 88.1% mips = 880, bj = 7423" 
-      if (options.tracefile == "qsort"):     print "qsort = 74.6% mips = 12950 bj = 50908"
-      if (options.tracefile == "towers"):    print "Towers = 96.3% mips = 21 bj = 574"
-      if (options.tracefile == "dhrystone"): print "dhrystone = 99.8%, misp = 39, bj = 22518"
-      if (options.tracefile == "vvadd"):     print "Vvadd = 97.3%, misp = 8, bj= 302, "
+      if (options.benchmark == "median"):    print "Median = 82.5% misp = 330, bj = 1888"
+      if (options.benchmark == "multiply"):  print "Multiply = 88.1% mips = 880, bj = 7423" 
+      if (options.benchmark == "qsort"):     print "qsort = 74.6% mips = 12950 bj = 50908"
+      if (options.benchmark == "towers"):    print "Towers = 96.3% mips = 21 bj = 574"
+      if (options.benchmark == "dhrystone"): print "dhrystone = 99.8%, misp = 39, bj = 22518"
+      if (options.benchmark == "vvadd"):     print "Vvadd = 97.3%, misp = 8, bj= 302, "
 
    if (options.debug):
       print pred
