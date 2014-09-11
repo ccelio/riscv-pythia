84eb7767243c440221c31cb43249106e903128e7
Christopher Celio <celio@eecs.berkeley.edu>
2014-09-10 18:22:15 -0700

    BTB refactored

--- btb.py
+++ btb.py
@@ -1,13 +1,11 @@
+from collections import OrderedDict
 
 class BTB:
 
-   btb = {}
-   history = []
-                    
    def __init__(self,w,n,default):
+      self.btb = OrderedDict()
       self.width = w
       self.num_entries = n
-      self.num_avail = n
       self.default_pkt = default
 
    def predict(self, pc):
@@ -16,26 +14,14 @@ class BTB:
       else:
          return (False, self.default_pkt)
 
-#         print "\nHIT! PC 0x%08x (%d), btb=%s" % (pc, pc, self.btb)
-#         print "\n     PC 0x%08x (%d), btb=%s miss... -.-" % (pc, pc, self.btb)
-
    def update(self, pc, taken, target):
       if (not taken):
          return 
-      if pc in self.btb:
-         self.btb[pc] = target
-         return
-      else:
-         if (self.num_avail <= 0):
-            old_pc = self.history.pop(0) # LRU replacement
-            del self.btb[old_pc]
-         else:
-            self.num_avail -= 1
-         self.history.append(pc)
-         self.btb[pc] = target
-         return
-       
+      self.btb[pc] = target
+      if len(self.btb) > self.num_entries:
+         self.btb.popitem(last=False)
 
    def display(self):
       print "BTB(%d): %d entries" % (self.width, self.num_entries)
       print self.btb
+
